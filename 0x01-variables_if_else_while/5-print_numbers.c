35f2ef484126824cf9dfbfb9141d57dcf4484505
albymenx08 <albymenx@gmail.com>
2023-04-21 22:28:14 -0700

    single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description : "prints the alphabet in lowercase, and then in uppercase"
+ *Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
